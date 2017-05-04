8a96d08bfe011c11bf561db6720611a053d21a7c
xiangcatherine <xiang.catherine@gmail.com>
2017-05-03 22:17:44 -0400

    "Builds JS functions that hide sign up/in forms until user clicks."

---
+++ assets/scripts/userexperience/main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const signUpAppear = function () {
+  $('#sign-up').removeClass('hidden')
+}
+
+const signInAppear = function () {
+  $('#sign-in').removeClass('hidden')
+}
+
+module.exports = {
+  signUpAppear,
+  signInAppear
+}
