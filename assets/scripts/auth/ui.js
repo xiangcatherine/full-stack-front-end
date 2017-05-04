73c96167f238c17dd0f0ab2ebcae4f2d69cade61
xiangcatherine <xiang.catherine@gmail.com>
2017-05-03 23:56:59 -0400

    Builds full sign-in function

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -5,13 +5,36 @@ const store = require('../store.js')
 const signUpSuccess = (data) => {
   console.log(data)
   $('#sign-up').addClass('hidden')
+  $('#register').addClass('hidden')
 }
 
 const signUpFailure = (error) => {
   console.error(error)
 }
 
+const signInSuccess = (data) => {
+  // store the user object:
+  store.user = data.user
+  console.log(data)
+  $('#sign-in').addClass('hidden')
+  $('#register').addClass('hidden')
+  $('#log-in').addClass('hidden')
+  $('#change-password').removeClass('hidden')
+  $('#sign-out').removeClass('hidden')
+  if ($('#sign-up').not('hidden')) {
+    $('#sign-up').addClass('hidden')
+  }
+}
+
+const signInFailure = (error) => {
+  console.log(error)
+  console.log('Either the username or password is wrong.')
+  $('.signInErrorMessage').text('Sorry, either the username or password was incorrect.')
+}
+
 module.exports = {
   signUpSuccess,
-  signUpFailure
+  signUpFailure,
+  signInSuccess,
+  signInFailure
 }
