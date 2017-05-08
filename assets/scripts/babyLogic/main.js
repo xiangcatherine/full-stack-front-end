ce1b7a2a2f12515ab84530d0acec758b38d42be6
xiangcatherine <xiang.catherine@gmail.com>
2017-05-07 22:10:38 -0400

    Adds more text updates

--- assets/scripts/babyLogic/main.js
+++ assets/scripts/babyLogic/main.js
@@ -60,10 +60,67 @@ const getBabySizeText = function (response) {
       updateText = 'Week 19: Baby is as big as a palmier cookie!'
       break
     case 20:
-      updateText = 'Week 20: Baby is as big as a raspberry!'
+      updateText = 'Week 20: Baby is as big as a banana!'
+      break
+    case 21:
+      updateText = 'Week 21: Baby is as big as a bottle of sriracha!'
+      break
+    case 22:
+      updateText = 'Week 22: Baby is as big as a coconut!'
+      break
+    case 23:
+      updateText = 'Week 23: Baby is as big as a burrito!'
+      break
+    case 24:
+      updateText = 'Week 24: Baby is as big as a demi-baguette!'
+      break
+    case 25:
+      updateText = 'Week 25: Baby is as big as a wide-brimmed fedora!'
+      break
+    case 26:
+      updateText = 'Week 26: Baby is as big as a bowling pin!'
+      break
+    case 27:
+      updateText = 'Week 27: Baby is as big as a terrarium!'
+      break
+    case 28:
+      updateText = 'Week 28: Baby is as big as a rollerblade!'
+      break
+    case 29:
+      updateText = 'Week 29: Baby is as big as a pineapple!'
+      break
+    case 30:
+      updateText = 'Week 30: Baby is as big as a zucchini!'
+      break
+    case 31:
+      updateText = 'Week 31: Baby is as big as a large movie popcorn!'
+      break
+    case 32:
+      updateText = 'Week 32: Baby is as big as a bento box!'
+      break
+    case 33:
+      updateText = 'Week 33: Baby is as big as a small chihuahua!'
+      break
+    case 34:
+      updateText = 'Week 34: Baby is as big as a rollerblade!'
+      break
+    case 35:
+      updateText = 'Week 35: Baby is as big as a picnic basket!'
+      break
+    case 36:
+      updateText = 'Week 36: Baby is as big as a pomenarian!'
+      break
+    case 37:
+      updateText = 'Week 37: Baby is as big as a honeydew melon!'
+      break
+    case 38:
+      updateText = 'Week 38: Baby is as big as a ukelele!'
+      break
+    case 39:
+      updateText = 'Week 39: Baby is as big as a beach ball!'
       break
     case 40:
-      updateText = 'Week 40: Baby is as big as a watermelon!'
+      updateText = 'Week 40: Baby is as big as a pikachu!'
       break
   }
 
