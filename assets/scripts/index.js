592b69b273983f2097cb43769a240a27470fa7ab
xiangcatherine <xiang.catherine@gmail.com>
2017-05-09 20:59:22 -0400

    Removes extra comments

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -20,8 +20,4 @@ window.onload = function () {
   document.body.removeAttribute('class')
 }
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
 require('./example')
